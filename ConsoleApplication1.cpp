de8d743056bbb5193b40a3523b840b5241cd2e8e
Ruslan A. Zarechnev <zra@corp.zdravalt.ru>
2016-09-07 12:39:11 +0700

    Работает. Алгоритм очистки нужно править. Что бы удалялись теги, возможно, эвристический анализ нужно...

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -10,58 +10,56 @@
 
 using namespace std;
 
-vector<string> arr; // mass of html file
+vector<string> arr;
 
 struct Params {
-	unsigned int f_index, count; // first index and index counts
+	unsigned int f_index, count;
 };
 
 DWORD WINAPI thread_func(void *p) {
 	unsigned int first_index = ((Params *)p)->f_index;
 	unsigned int index_counts = ((Params *)p)->count;
-	for (unsigned int k = first_index; k < first_index + index_counts; k++) {
-
-		//cycle_BEGIN
+	for (unsigned int k = first_index; k < first_index + index_counts; ++k) {
 		string text = "";
 		char ch = '\0';
-		bool b = false;
-
+		bool b = true;
 		for (unsigned int i = 0; i < arr[k].length(); i++) {
-			if (ch == '>') b = true;
-			if (b) {
-				if (ch != '<' && ch != '>') text += ch;
-			}
+			ch = arr[k][i];
 			if (ch == '<') b = false;
+			if (ch == '>') {
+				b = true;
+				continue;
+			}
+			if (b) text += ch;
 		}
 		arr[k] = text;
-		//cycle_END
 	}
-
 	return 0;
 }
 
 int main(int argc, char* argv[]) {
-	ifstream html_file("F:\\vs\\лр_1-многопоточ\\html-delete-multythread\\Debug\\1.txt");
-	unsigned int thread_count = 2;
+	ifstream html_file;
+	ofstream out_file;
+	unsigned int thread_count;
 	
-	//if (argc > 1) {
-	//	html_file.open(argv[1]);
-	//	if (!html_file.is_open()) {
-	//		cout << "HTML file can not be opened." << endl;
-	//		return -1;
-	//	}
-	//	if (argv[2]) thread_count = int(argv[2]);
-	//	else thread_count = 2;
-	//}
-	//else {
-	//	cout << "Enter HTML file, please." << endl;
-	//	system("pause");
-	//	return -2;
-	//}
+	if (argc > 1) {
+		html_file.open(argv[1]);
+		if (!html_file.is_open()) {
+			cout << "HTML file can not be opened." << endl;
+			return -1;
+		}
+		if (argv[2]) thread_count = unsigned int(argv[2]);
+		else thread_count = 2;
+	}
+	else {
+		cout << "Enter HTML file, please." << endl;
+		system("pause");
+		return -2;
+	}
 	
-	string str;
+	out_file.open("out.txt", ios::trunc);
 
-	ofstream out_file("out.txt");
+	string str;
 
 	while (!html_file.eof()) {
 		getline(html_file, str);
@@ -76,17 +74,15 @@ int main(int argc, char* argv[]) {
 
 	for (unsigned int k = 0; k < thread_count; k++) {
 		//cout << "thread" << endl;
-		int count_el_one_thr = arr.size() / thread_count;
+		unsigned int count_el_one_thr = arr.size() / thread_count;
 		//first element index
 		thread_args[k].f_index = k * count_el_one_thr;
 		//elements count
-		thread_args[k].count = count_el_one_thr;
+		thread_args[k].count = (count_el_one_thr < (arr.size() - count_el_one_thr * k) ? count_el_one_thr : (arr.size() - count_el_one_thr * k));
 		threadProducer[k] = CreateThread(0, 0, &thread_func, &thread_args[k], 0, 0);
 	}
 
 	WaitForMultipleObjects(thread_count, threadProducer, true, INFINITE);
-
-	system("pause");
 
 	for (unsigned int i = 0; i < arr.size(); i++) {
 		out_file << arr[i] << endl;
