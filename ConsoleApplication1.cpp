5b4a8bf4badb6e5b99dc8e3fecea96946e738187
ruslan <zarechnev.r@yandex.ru>
2016-09-09 12:37:49 +0700

    не работает.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -14,14 +14,16 @@ using namespace std;
 vector<string> arr;
 
 struct Params {
-	unsigned int f_index, count;
-	vector<string> *thr_index_addr;
+	unsigned int f_index, count, thread_index;
+	vector<string> *thr_ans_vector_addr;
 };
 
 DWORD WINAPI thread_func(void *p) {
 	unsigned int first_index = ((Params *)p)->f_index;
 	unsigned int index_counts = ((Params *)p)->count;
-	for (unsigned int k = first_index; k < first_index + index_counts; ++k) {
+	unsigned int thred_index = ((Params *)p)->thread_index;
+	vector<string> *ans = ((Params *)p)->thr_ans_vector_addr;
+	for (unsigned int k = first_index, j = 0; k < first_index + index_counts; ++k, j++) {
 		string text = "";
 		char ch = '\0';
 		bool b = true;
@@ -34,7 +36,7 @@ DWORD WINAPI thread_func(void *p) {
 			}
 			if (b) text += ch;
 		}
-		arr[k] = text;
+		ans[thred_index].push_back(text);
 	}
 	return 0;
 }
@@ -56,7 +58,7 @@ int main(int argc, char* argv[]) {
 	}
 	
 	else {
-		html_file.open("in.txt");
+		html_file.open("D:\\in.txt");
 		if (!html_file.is_open()) {
 			cout << "HTML file can not be opened." << endl;
 			cout << "Enter HTML file, please." << endl;
@@ -73,9 +75,7 @@ int main(int argc, char* argv[]) {
 		getline(html_file, str);
 		arr.push_back(str);
 	}
-	cout << clock() - t << " - arr created." << endl;
 
-	//threads_count <= strings in file
 	if (thread_count > arr.size()) thread_count = arr.size();
 	
 	HANDLE *threadProducer = new HANDLE[thread_count]; // threads
@@ -89,7 +89,8 @@ int main(int argc, char* argv[]) {
 		//first element index
 		thread_args[k].f_index = k * count_el_one_thr;
 		//elements count
-		thread_args[k].thr_index_addr = &ans[k];
+		thread_args[k].thr_ans_vector_addr = &ans[k];
+		thread_args[k].thread_index = k;
 		thread_args[k].count = (count_el_one_thr < (arr.size() - count_el_one_thr * k) ? count_el_one_thr : (arr.size() - count_el_one_thr * k));
 		threadProducer[k] = CreateThread(0, 0, &thread_func, &thread_args[k], 0, 0);
 	}
@@ -97,8 +98,8 @@ int main(int argc, char* argv[]) {
 	WaitForMultipleObjects(thread_count, threadProducer, true, INFINITE);
 
 	for (unsigned int i = 0; i < thread_count; i++) {
-		for (unsigned int k = 0; k < ans[i].size(); k ++)
-				out_file << ans[i][k] << endl;
+		for (unsigned int k = 0; k < ans[i].size(); k++)
+			out_file << ans[i].size() << endl;
 	}
 
 	//for (unsigned int i = 0; i < arr.size(); i++) {
